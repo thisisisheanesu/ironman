127a3415038835428018ea75e67612b85845953d
Isheanesu Misi <inmisimail@gmail.com>
2024-06-05 15:57:21 +0200

    chore: Add router for vercel

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './App.css'
+// import * as THREE from 'three'
+import { Router, Routes, Route } from 'react-router-dom'
+import { Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, ContactShadows, OrbitControls } from '@react-three/drei'
+import Ironman from './lib/model'
+import Main from './layouts/main'
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <>      
+              <Main />
+              <div className="canvas-container">
+                <Canvas camera={{ position: [0, 0, 0], fov: 55 }}>
+                  <pointLight position={[10, 10, 10]} intensity={1.5} />
+                  <Suspense fallback={null}>
+                    <group rotation={[4.5, -0.3, 8.4]} position={[3.7, 0, -5]} scale={0.8}>
+                      <Ironman />
+                    </group>
+                    <Environment preset="city" />
+                  </Suspense>
+                  <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
+                  <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} />
+                </Canvas>
+              </div>
+            </>} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
